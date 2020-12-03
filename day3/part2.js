e8297c21871d68ed3b962c3598bc8ddea602f06f
Kathrin Holzmann <lelith@gmx.de>
2020-12-03 17:06:15 +0100

    part 2 solved

--- day3/part2.js
+++ day3/part2.js
@@ -1,11 +1,10 @@
 const utils = require('../utils');
 
-function countTrees(data) {
+function countTrees(data, rowJump, lineJump) {
   let pointer = 0;
   let amountTrees = 0;
-  const rowJump = 3;
-  data.forEach((mapRow, index) => {
-    const rowArr = mapRow.split('');
+  for (let index = 0; index < data.length; index += lineJump) {
+    const rowArr = data[index].split('');
     // console.log(`we are in row ${index} and look at ${pointer}`);
     // console.log(rowArr);
     if (rowArr[pointer] === '#' && index !== 0) {
@@ -13,20 +12,36 @@ function countTrees(data) {
       amountTrees += 1;
     }
     if (pointer + rowJump >= rowArr.length) {
-      pointer = (pointer + rowJump) - rowArr.length;
+      pointer = (pointer + rowJump) % rowArr.length;
     } else {
       pointer += rowJump;
     }
-  });
-
+  }
+  console.log(`for ${rowJump} and ${lineJump} the navigationSystem has encounterd ${amountTrees} trees`);
   return amountTrees;
+}
+
+function navigationSystem(data) {
+  const slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+  ];
+  const amountTrees = [];
+  slopes.forEach((slope) => {
+    amountTrees.push(countTrees(data, slope[0], slope[1]));
+  });
+  const sumTrees = amountTrees.reduce((a, b) => a * b);
+  return sumTrees;
 }
 
 try {
   // let data = utils.readInput('./example.txt');
   let data = utils.readInput('./input.txt');
   data = utils.modDataNewlineStr(data);
-  const amountTrees = countTrees(data);
+  const amountTrees = navigationSystem(data);
   console.log(`the navigation system encountered ${amountTrees} trees`);
 } catch (e) {
   console.log('Error', e.stack);
