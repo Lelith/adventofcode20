87a0bdeb2cf570a125c8c5b6e7b77917ff141d6a
Kathrin Holzmann <lelith@gmx.de>
2020-12-07 11:43:04 +0100

    finished part1

--- day7/part1.js
+++ day7/part1.js
@@ -3,22 +3,35 @@ const utils = require('../utils');
 function saveInstructions(data) {
   const instructions = {};
   data.forEach((rule) => {
-    console.log(rule);
-    let bags = rule.split(/\d/g).map(splitted => splitted.trim());
-    bags = bags.map(bag => bag.split(' ', 2).join(''));
+    let bags = rule.split(/\d/g).map((splitted) => splitted.trim());
+    bags = bags.map((bag) => bag.split(' ', 2).join(''));
     if (bags.length > 1) {
       const mainBag = bags.shift();
       instructions[mainBag] = bags;
     }
   });
-  console.log(instructions);
+  return instructions;
+}
+
+function findBags(instructions, searchTerm, possibleBags) {
+  return Object.keys(instructions).map((bag) => {
+    if (instructions[bag].indexOf(searchTerm) > -1) {
+      if (possibleBags.indexOf(bag) === -1) {
+        possibleBags.push(bag);
+        findBags(instructions, bag, possibleBags);
+      }
+    }
+  });
 }
 
 try {
-  let data = utils.readInput('./example.txt');
-  // let data = utils.readInput('./input.txt');
+  // let data = utils.readInput('./example.txt');
+  let data = utils.readInput('./input.txt');
   data = utils.modDataNewlineStr(data);
-  saveInstructions(data);
+  const instructions = saveInstructions(data);
+  const possibleBags = [];
+  findBags(instructions, 'shinygold', possibleBags);
+  console.log(possibleBags.length);
 } catch (e) {
   console.log('Error', e.stack);
 }
