3f11e6273737179fc995e5a9423687cde91908cf
Kathrin Holzmann <lelith@gmx.de>
2020-12-10 18:30:51 +0100

    remove unused code

--- day10/part2.js
+++ day10/part2.js
@@ -1,25 +1,5 @@
 const utils = require('../utils');
 
-function findDifferences(data, start) {
-  let num3 = 0;
-  let num1 = 0;
-  data.forEach((item, index) => {
-    if (index === data.length - 1) {
-      num3 += 1;
-    } else {
-      let diff = 0;
-      diff = data[index + 1] - item;
-      if (diff === 1) {
-        num1 += 1;
-      } else if (diff === 3) {
-        num3 += 1;
-      }
-    }
-  });
-  console.log(`num1 differences: ${num1} num3 differences: ${num3}`);
-  console.log(num1 * num3);
-}
-
 function findSequence(data) {
   const sequenceLengths = [];
   let sumSequence = 0;
@@ -57,7 +37,7 @@ try {
   data = utils.modDataNewline(data);
   data = data.sort((a, b) => a - b);
   data.unshift(0);
-  data.push(data[data.length-1] + 3);
+  data.push(data[data.length - 1] + 3);
   console.log(data);
   const sequences = findSequence(data);
   const fibonaccis = sequences.map(item => fib3Step(item));
