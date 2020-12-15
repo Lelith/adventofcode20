7b76fe3648e6862152c629a8cc4d4ba6129b107e
Kathrin Holzmann <lelith@gmx.de>
2020-12-15 18:41:32 +0100

    remove second array

--- day15/part2.js
+++ day15/part2.js
@@ -1,28 +1,27 @@
 function playMemory() {
-  const numbers = ['X', 0, 3, 6];
+  let lastCall = 6;
   const uniqueObj = {
     0: [1],
     3: [2],
     6: [3],
   };
-  for (let rounds = numbers.length; rounds <= 30000000; rounds += 1) {
-    const lastNumberCalled = numbers[rounds - 1];
-    const lastNumberObj = uniqueObj[lastNumberCalled];
+  for (let rounds = 4; rounds <= 2020; rounds += 1) {
+    const lastNumberObj = uniqueObj[lastCall];
     if (lastNumberObj.length > 1) {
       const arrLength = lastNumberObj.length;
       const newNumber = lastNumberObj[arrLength - 1] - lastNumberObj[arrLength - 2];
-      numbers[rounds] = newNumber;
+      lastCall = newNumber;
       if (uniqueObj[newNumber] !== undefined) {
         uniqueObj[newNumber].push(rounds);
       } else {
         uniqueObj[newNumber] = [rounds];
       }
     } else {
-      numbers[rounds] = 0;
+      lastCall = 0;
       uniqueObj[0].push(rounds);
     }
   }
-  console.log(numbers[30000000]);
+  console.log(lastCall);
 }
 
 try {
